59a33420efdc6227e071a1edd2c910727d7d6738
Aleksey <konanwar@yandex.ru>
2026-09-22 08:26:23 +0300

    This is the working code

--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,17 @@
 
 
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+input_string = str(input())
+identifier = ''
+_digits = ''
+for i in input_string:
+   if i.isalpha():
+     identifier += i
+   elif i.isdigit():
+     _digits += i
+if identifier == 'Счет':
+    print(identifier, get_mask_account(_digits))
+else:
+    print(identifier, get_mask_card_number(_digits))
