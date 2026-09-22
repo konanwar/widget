1b0365f939e85498e86c95258e7e6bd572864c68
Aleksey <konanwar@yandex.ru>
2026-09-22 08:35:38 +0300

    We format it as a functionс

--- src/widget.py
+++ src/widget.py
@@ -3,15 +3,21 @@
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
+
+def mask_account_card(input_string: str) -> str:
+   """определяем что на входе(счет,карта)"""
+   identifier = ''
+   _digits = ''
+   for i in input_string:
+      if i.isalpha():
+         identifier += i
+      elif i.isdigit():
+         _digits += i
+   if identifier == 'Счет':
+      print(identifier, get_mask_account(_digits))
+   else:
+      print(identifier, get_mask_card_number(_digits))
+
+
 input_string = str(input())
-identifier = ''
-_digits = ''
-for i in input_string:
-   if i.isalpha():
-     identifier += i
-   elif i.isdigit():
-     _digits += i
-if identifier == 'Счет':
-    print(identifier, get_mask_account(_digits))
-else:
-    print(identifier, get_mask_card_number(_digits))
+mask_account_card(input_string)
