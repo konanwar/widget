39cdb6ecdf9b1b4143ebdf0f559b27045a7d9cbc
Aleksey <konanwar@yandex.ru>
2026-09-24 05:40:43 +0300

    accepted by the mentor

--- src/widget.py
+++ src/widget.py
@@ -13,27 +13,27 @@ def mask_account_card(input_string: str) -> str:
         elif i.isdigit():
             _digits += i
     if identifier == "Счет":
-        print(identifier, get_mask_account(_digits))
+        return identifier + get_mask_account(_digits)
     else:
-        print(identifier, get_mask_card_number(_digits))
+        return identifier + get_mask_card_number(_digits)
 
 
 def get_date(input_data: str) -> str:
     """передает в требуемом формате дату по заданию"""
     dt = datetime.strptime(input_data, "%Y-%m-%dT%H:%M:%S.%f")
-    print(dt.strftime("%d.%m.%Y"))
+    return dt.strftime("%d.%m.%Y")
 
 
 def get_date_now(input_data: str) -> str:
     """передает  дату ввода реквизитов"""
     dt = datetime.strptime(input_data, "%Y-%m-%d %H:%M:%S.%f")
-    print(dt.strftime("%d.%m.%Y"))
+    return dt.strftime("%d.%m.%Y")
 
 
 input_string = str(input("ведите реквизиты"))
 input_data = str(input("введите дату"))
-mask_account_card(input_string)
+print(mask_account_card(input_string))
 input_data_now = str(datetime.now())
-get_date(input_data)
-get_date_now(input_data_now)
+print(get_date(input_data))
+print(get_date_now(input_data_now))
 # print(input_data)
